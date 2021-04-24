cde35fe4a31c92267e4c16f6d8e58456b6dbdeb8
Malgorzata Smolarek-Godlewska <malgorzatasmolarek-godlewska@MacBook-Air-Malgorzata.local>
2021-04-24 22:25:01 +0200

    Initial commit

--- src/test/java/com/gosia/multiplication/MultiplicationApplicationTests.java
+++ src/test/java/com/gosia/multiplication/MultiplicationApplicationTests.java
@@ -1,13 +1,39 @@
 package com.gosia.multiplication;
 
+import com.gosia.multiplication.controller.LoginController;
+import com.gosia.multiplication.controller.QuestionController;
+import org.assertj.core.api.Assertions;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class MultiplicationApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Autowired
+	private LoginController loginController;
+
+	@Autowired
+	private QuestionController questionController;
+
+	@Before
+	public void init(){
+		System.out.println("Start");
 	}
 
+	@Test
+	void contextLoads() {
+		Assertions.assertThat(loginController).isNotNull();
+		Assertions.assertThat(questionController).isNotNull();
+	}
+
+	@After
+	public void done(){
+		System.out.println("Koniec");
+	}
 }
